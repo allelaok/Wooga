3816f4277d723e037d5f5ecef171cd054c24979e
Hercules1226 <86274513+Hercules1226@users.noreply.github.com>
2021-10-18 16:12:32 +0900

    합치는 파일 다시 고쳐서 올림

--- Wooga/Source/Wooga/Wooga.Build.cs
+++ Wooga/Source/Wooga/Wooga.Build.cs
@@ -8,7 +8,7 @@ public class Wooga : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "AIModule", "NavigationSystem" });
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "AIModule", "NavigationSystem", "UMG" });
 
         PrivateDependencyModuleNames.AddRange(new string[] { "HeadMountedDisplay" });
 
